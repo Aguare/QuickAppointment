06786563ee50975fb01719a78aef45cc8214d406
Aguare <marcosy300@gmail.com>
2024-10-19 15:31:00 -0600

    Init backend and jenkinsfile

---
+++ app-backend/src/test/java/com/example/app_backend/AppBackendApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testMain() {
+		AppBackendApplication.main(new String[] {});
+	}
+
+}
